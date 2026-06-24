432190dcc2ac83428b522b608fcfdb47f339277a
fxmidaaa <rustemcimendur123@gmail.com>
2026-06-24 23:16:10 +0300

    second problem done (valid_anagram)

---
+++ valid_anagram.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+            
+        char_count = {}
+        
+        for i in range(len(s)):
+            char_count[s[i]] = char_count.get(s[i], 0) + 1
+            char_count[t[i]] = char_count.get(t[i], 0) - 1
+
+        for count in char_count.values():
+            if count != 0:
+                return False
+                
+        return True
