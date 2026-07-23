16d11d7009c24fa2cd32c412e6b0ce528d66cbbb
fxmidaaa <rustemcimendur123@gmail.com>
2026-07-23 14:29:24 +0300

    1 medium (stack) + 1 easy (arrays & hashing)

---
+++ arrays_hashing/majority_element.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        seen = {}
+
+        for num in nums:
+            seen[num] = seen.get(num, 0) + 1
+            if seen[num] > len(nums) // 2:
+                return num
