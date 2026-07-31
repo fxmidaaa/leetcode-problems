9240b5002a238e7cc06d20aaaa522445cd17b38c
fxmidaaa <rustemcimendur123@gmail.com>
2026-07-31 16:06:15 +0300

    +1 two pointers

---
+++ two_pointers/remove_dups_from_sorted_array.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        left = 0
+        for right in range(1, len(nums)):
+            if nums[right] != nums[left]:
+                left += 1
+                nums[left] = nums[right]
+        return left + 1
